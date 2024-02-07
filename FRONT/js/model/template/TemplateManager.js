41729dceb07062528449d0450fc9e823cafcd545
Alix Jaugey <jaugey.alix@gmail.com>
2024-02-07 09:52:08 +0100

    Remove path variable and add onClick method to TemplateManager

--- FRONT/js/model/template/TemplateManager.js
+++ FRONT/js/model/template/TemplateManager.js
@@ -9,7 +9,6 @@ class TemplateManager {
      */
     constructor(container = null) {
         this.container = container;
-        this.path = 'visual/templates/DocumentTemplate.html';
     }
 
     /**
@@ -41,5 +40,34 @@ class TemplateManager {
     async addTemplate(values) {
         // add the template to the container with the values
         this.container.innerHTML += await Utils.readAndFormatFile(this.path, values);
+    }
+
+    /**
+     * Action to be performed when the element with the specified class is clicked (when they contain the 'data-id' attribute)
+     * @param {lambda} callback Callback to be called when the element with the specified class is clicked
+     * @param {string} className Name of the class to be clicked to call the callback 
+     * @param {string} parentClass the class of the parent of the element to be clicked
+     * @param {string} limitElement QuerySelector string to limit the search (ex: 'body' to search up to the body element or '.container' to search up to the container element)
+     * @Note If the className is empty, the callback will be called when an element with the parentClass is clicked
+     */
+    onClick(callback, className, parentClass, limitElement = 'main') {
+        // add the event listener to the container
+        this.container.addEventListener('click', (event) => {
+            // get the target of the event
+            let target = event.target;
+
+            // check if the target has the specified class (if the class is not empty otherwise pass)
+            if (!target.classList.contains(className) && className !== '') {
+                return;
+            }
+
+            // get the parent of the target with the specified class
+            let parentWithClass = Utils.getParentWithClass(target, parentClass, limitElement);
+
+            // if the parent exists, call the callback with the id of the parent otherwise do nothing
+            if (parentWithClass && parentWithClass.getAttribute('data-id')) {
+                callback(parentWithClass.getAttribute('data-id'));
+            }
+        });
     }
 }
