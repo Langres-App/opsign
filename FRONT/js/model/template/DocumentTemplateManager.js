475f3913879fef64aae47a306c1a7492ff3d9da3
Alix Jaugey <jaugey.alix@gmail.com>
2024-02-07 09:52:20 +0100

    Add DocumentTemplateManager class for managing document templates

---
+++ FRONT/js/model/template/DocumentTemplateManager.js
@@ -0,0 +1,45 @@
+/**
+ * @class DocumentTemplateManager - This class is responsible for managing the document template
+ */
+class DocumentTemplateManager extends TemplateManager {
+
+    /**
+     * Constructor of the SignedUserTemplateManager
+     * @param {HTMLElement} container Container of the template manager
+     */
+    constructor(container = null) {
+        super(container);
+        this.path = 'visual/templates/DocumentTemplate.html';
+    }
+
+    /**
+     * Add a PoDocument to the container
+     * @param {PoDocument} document Document to be added to the container
+     * @returns Promise<void>
+     */
+    async addDocument(document) {
+        // values is a map that contains the values to be replaced in the template, 
+        // the key is the name of the variable in the template and the value is the value to replace
+        let values = [];
+
+        // check if the passed parameter is a Document object
+        if (document instanceof PoDocument) {
+            values['id'] = document.getId();
+            values['Title'] = document.getFileName();
+            const lastVer = document.getLastVersion();
+
+            // check if the last version exsists and is a Version object
+            if (!lastVer || !(lastVer instanceof Version)) return;
+
+            values['Date'] = lastVer.getAddDate();
+            values['Preview'] = lastVer.getPreview();
+        }
+
+        // call parent method to add the document to the container
+        await super.addTemplate(values);
+    }
+
+    onDocumentClicked(callback) {
+        super.onClick(callback, '', 'document', '#list');
+    }
+}
