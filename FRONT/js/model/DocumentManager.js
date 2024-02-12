8bc7c4f0f674f35839f241839dbbf19fbd08f1a5
Alix Jaugey <jaugey.alix@gmail.com>
2024-02-12 09:31:35 +0100

    Refactor DocumentManager class to use inheritance and add CRUD methods that convert responses to document when getting the data

--- FRONT/js/model/DocumentManager.js
+++ FRONT/js/model/DocumentManager.js
@@ -1,48 +1,138 @@
-class DocumentManager {
+/**
+ * @class DocumentManager - Manages the documents in the system
+ */
+class DocumentManager extends DataManager {
 
-    static #dao;
+    constructor() {
+        this.#init();
+    }
 
-    static async #init() {
-        if (DocumentManager.#dao === undefined) {
+    /**
+     * Initializes the DocumentManager class
+     */
+    async #init() {
+        if (super.getDao() === undefined) {
             await Utils.addScript('js/data/dao/Dao.js');
             await Utils.addScript('js/data/dao/DocumentDao.js');
-            DocumentManager.#dao = new DocumentDao();
+            super.setDao(new DocumentDao());
         }
     }
+    /**
+     * Returns all the object
+     * @returns {Array} - An array of all the object
+     */
+    async getAll() {
+        let toReturn = [];
+        let data;
 
-    static async #getDao() {
-        await DocumentManager.#init();
-        return DocumentManager.#dao;
-    }
-    
-    static async getAllDocuments() {
-        let dao = await DocumentManager.#getDao();
-        return await dao.getAll();
-    }
+        // we try to get the data
+        try {
+            data = await super.getAll();
+        } catch (e) {
+            console.log(e);
+        }
 
-    static async getDocumentById(id) {
-        let dao = await DocumentManager.#getDao();
-        return await dao.getById(id);
-    }
-    
-    static async addDocument(document) {
-        let dao = await DocumentManager.#getDao();
-        await dao.add(document);
-    }
+        // if no error was thrown and the data is defined we return what the function needs to
+        if (data !== undefined) {
+            data.forEach(doc => {
+                toReturn.push(new PoDocument(doc))
+            });
 
-    static async updateDocument(document) {
-        let dao = await DocumentManager.#getDao();
-        await dao.update(document);
-    }
+            return toReturn;
+        }
 
-    static async addVersionToDocument(documentId, version) {
-        let dao = await DocumentManager.#getDao();
-        await dao.addVersion(documentId, version);
+        return null;
     }
 
-    static async deleteDocument(id) {
-        let dao = await DocumentManager.#getDao();
-        await dao.delete(id);
+    /**
+     * Returns the object with the given id
+     * @param {number | string} id identifier of the object
+     * @returns object with the given id
+     */
+    async getById(id) {
+        let data;
+
+        try {
+            data = await this.getById(id);
+        } catch (e) {
+            console.log(e);
+        }
+
+        // if no error was thrown and the data is defined we return what the function needs to
+        if (data !== undefined) {
+            return new PoDocument(data);
+        }
+
+        return null;
     }
+
+    /**
+     * Returns the object with the given name
+     * @param {string} name name of the object
+     */
+    async add(object) {
+        let data;
+
+        try {
+            data = await this.add(object);
+        } catch (e) {
+            console.log(e);
+        }
+
+        // depending on what appened we either reload the page or return a message
+        if (data) { }  // TODO message popup that reload the view
+    }
+
+    /**
+     * Adds a version to the given document
+     * @param {number | string} documentId document identifier
+     * @param {version} version version to add to the document 
+     */
+    async addVersion(documentId, version) {
+        let dao = await this.getDao();
+        let data;
+
+        try {
+            data = await dao.addVersion(documentId, version);
+        } catch (e) {
+            console.log(e);
+        }
+
+        // depending on the api call, either tell the element was added, or return an error message
+        if (data) { } // TODO message popup that reload the view / error message
+    }
+
+
+    /**
+     * Updates the given object
+     * @param {object} object Object to update
+     */
+    async update(object) {
+        let data;
+        try {
+            data = await this.update(object);
+        } catch (e) {
+            console.log(e);
+        }
+
+        if (data) {} // TODO update view / reload page
+    }
+
+    /**
+     * Deletes the object with the given id
+     * @param {number | string} id identifier of the object to delete  
+     */
+    async delete(id) {
+        let data;
+
+        try {
+            data = await this.delete(id);
+        } catch (e) {
+            console.log(e);
+        }
+
+        if (data) {} // TODO update view / reload page
+    }
+
 
 }
