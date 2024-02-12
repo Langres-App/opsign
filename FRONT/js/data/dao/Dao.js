d0a5659b4277de9ca3676bc866916dfe9eafcb21
Alix Jaugey <jaugey.alix@gmail.com>
2024-02-12 08:25:43 +0100

    Add documentation to the Dao class

--- FRONT/js/data/dao/Dao.js
+++ FRONT/js/data/dao/Dao.js
@@ -1,8 +1,15 @@
+/**
+ * @class Dao - This class is the parent class of all the dao classes. It contains the basic CRUD operations.
+ */
 class Dao {
+    /**
+     * Create a new Dao object to interact with the api (CRUD operations)
+     * @param {string} endpoint endpoint of the api to use
+     */
     constructor(endpoint) {
         // set the url of the api
         this.url = "http://http://slanlp0033.ad.ponet/charteapi/";
-        
+
         // check if the endpoint is defined
         if (endpoint === undefined) {
             throw new Error("You must provide an endpoint");
@@ -12,18 +19,32 @@ class Dao {
         this.endpoint = endpoint;
     }
 
+    /**
+     * Get all the objects in the database
+     * @returns All the objects in the database
+     */
     async getAll() {
         let response = await fetch(this.url + this.endpoint);
         let data = await response.json();
         return data;
     }
 
+    /**
+     * Get an object by its id
+     * @param {number} id Id of the object to get
+     * @returns The object with the given id as a json object
+     */
     async getById(id) {
         let response = await fetch(this.url + this.endpoint + "/" + id);
         let data = await response.json();
         return data;
     }
 
+    /**
+     * Add an object to the database
+     * @param {object} data The data to add to the database
+     * @returns The result of the add request
+     */
     async add(data) {
         let response = await fetch(this.url + this.endpoint, {
             method: 'POST',
@@ -36,6 +57,11 @@ class Dao {
         return result;
     }
 
+    /**
+     * Update an object in the database
+     * @param {object} data Data to update (must contain an id field)
+     * @returns The result of the update request
+     */
     async update(data) {
         let response = await fetch(this.url + this.endpoint + "/" + data.id, {
             method: 'PUT',
@@ -48,6 +74,11 @@ class Dao {
         return result;
     }
 
+    /**
+     * Delete an object from the database
+     * @param {number} id Id of the object to delete
+     * @returns the result of the delete request
+     */
     async delete(id) {
         let response = await fetch(this.url + this.endpoint + "/" + id, {
             method: 'DELETE'
