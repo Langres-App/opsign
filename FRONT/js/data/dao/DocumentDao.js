d6b75cc8e89d1278513b4287bab5d6b8992f0748
Alix Jaugey <jaugey.alix@gmail.com>
2024-02-12 08:24:59 +0100

    Add DocumentDao class with addVersion method and documentation

--- FRONT/js/data/dao/DocumentDao.js
+++ FRONT/js/data/dao/DocumentDao.js
@@ -1,9 +1,21 @@
+/**
+ * @class DocumentDao - Data Access Object for documents
+ */
 class DocumentDao extends Dao {
+  /**
+   * Create a new DocumentDao object to interact with the documents api
+   */
   constructor() {
+    // set the endpoint
     super('documents');
-    this.documents = [];
   }
 
+  /**
+   * Add a document to the database
+   * @param {number} id Id of the document to get
+   * @param {Version} versionToAdd Version to add to the document
+   * @returns The result of the addVersion request
+   */
   async addVersion(id, versionToAdd) {
     let response = await fetch(this.url + this.endpoint + '/' + id, {
       method: 'POST',
