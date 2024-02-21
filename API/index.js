a113dc582169630e1b1b85aa02627fc314273657
Alix Jaugey <jaugey.alix@gmail.com>
2024-02-21 16:07:24 +0100

    Update express body parser limits

--- API/index.js
+++ API/index.js
@@ -6,10 +6,10 @@ app.use(cors());
 
 const PORT = process.env.PORT;
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true, limit: '50mb'}))
+app.use(express.json({ limit: '50mb' }))
 
-app.get('/',
+app.get('/', 
     (req, res) => res.send('Welcome to the Document Management System API!'));
 
 app.listen(PORT,
