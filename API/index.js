a804668e57486686192fae35700b4cab5bd96156
Alix Jaugey <jaugey.alix@gmail.com>
2024-03-04 09:50:42 +0100

    index.js - added bodyparser

--- API/index.js
+++ API/index.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(cors());
 
 const PORT = process.env.PORT;
 
-app.use(express.urlencoded({ extended: true, limit: '50mb'}))
-app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 
 app.get('/', 
     (req, res) => res.send('Welcome to the Document Management System API!'));
 
 app.listen(PORT,
-    () => console.log(`[BOOTUP]: Server is running at port: ${PORT}`));
+    () => console.log(`[${new Date().toLocaleTimeString()}]: Server is running at port: ${PORT}`));
+
+
+app.use(bodyParser.raw({ type: 'application/octet-stream' }));
 
 // Import and use the routes
 app.use('/documents', require('./controllers/DocumentController.js'));
